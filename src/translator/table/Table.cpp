3739f62f4b1b642d07b96fc010954a4aa47ab92c
Arthur Sonzogni <sonzogniarthur@gmail.com>
2018-05-02 23:54:54 +0200

    Add Translator for frame and tree. Add theme.

---
+++ src/translator/table/Table.cpp
@@ -0,0 +1,569 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+#include "screen/Screen.h"
+#include "translator/Translator.h"
+
+namespace {
+  // clang-format off
+  //---------------------
+  // 
+  //    0   1   1   2
+  //  
+  // 0  ABBBCBBBCBBBD
+  //    E   F   F   G
+  // 1  HIIIJIIIJIIIK
+  //    L   M   M   N
+  // 2  OPPPQPPPQPPPR
+  //    L   M   M   N
+  // 2  OPPPQPPPQPPPR
+  //    L   M   M   N
+  // 3  STTTUTTTUTTTV
+  //
+  // A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q  R  S  T  U  V
+  // 0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20 21
+  // 
+  //---------------------
+  struct Style {
+    std::wstring charset[22];
+    int width[3];
+    int height[4];
+  };
+  
+  std::map<std::string, Style> styles = {
+    {
+      "ascii",
+      Style{
+        // charset
+        {
+          L"+" , L"-" , L"+" , L"+" ,
+          L"|"        , L"|" , L"|" ,
+          L"+" , L"-" , L"+" , L"+" ,
+
+          L"|"        , L"|" , L"|" ,
+          L"+" , L"-" , L"+" , L"+" ,
+
+          L"+" , L"-" , L"+" , L"+" ,
+        },
+        // width/height
+        {1,1,1}, {1,1,1,1},
+      }
+    },
+    {
+      "ascii rounded",
+      Style{
+        // charset
+        {
+          L"." , L"-" , L"+" , L"." ,
+          L"|"        , L"|" , L"|" ,
+          L"|" , L"-" , L"+" , L"|" ,
+
+          L"|"        , L"|" , L"|" ,
+          L"|" , L"-" , L"+" , L"|" ,
+
+          L"'" , L"-" , L"+" , L"'" ,
+        },
+        // width/height
+        {1,1,1}, {1,1,1,1},
+      }
+    },
+    {
+      "ascii with header 1",
+      Style{
+        // charset
+        {
+          L"|=" , L"=" , L"=" , L"=|" ,
+          L"| "        , L"|" , L" |" ,
+          L"|=" , L"=" , L"=" , L"=|" ,
+
+          L" |"        , L"|" , L"| " ,
+          L" +" , L"-" , L"+" , L"+ " ,
+
+          L" +" , L"-" , L"+" , L"+ " ,
+        },
+        // width
+        {2,1,2},{1,1,1,1},
+      }
+    },
+    {
+      "ascii with header 2",
+      Style{
+        // charset
+        {
+          L"=" , L"=" , L"=" , L"=" ,
+          L"|"        , L"|" , L"|" ,
+          L"=" , L"=" , L"=" , L"=" ,
+
+          L"|"        , L"|" , L"|" ,
+          L"+" , L"-" , L"+" , L"+" ,
+
+          L"+" , L"-" , L"+" , L"+" ,
+        },
+        // width
+        {1,1,1},{1,1,1,1},
+      }
+    },
+    {
+      "ascii light header",
+      Style{
+        // charset
+        {
+          L"" , L"" , L"" , L"" ,
+          L"" ,       L" " , L""  ,
+          L"" , L"-", L" " , L"" ,
+
+          L"" ,       L" ", L""  ,
+          L"" , L"",  L"" , L"" ,
+
+          L"" , L"",  L"" , L"" ,
+        },
+        // width
+        {0,1,0},{0,1,0,0},
+      }
+    },
+    {
+      "ascii light header/separator",
+      Style{
+        // charset
+        {
+          L"" , L"" , L"" , L"" ,
+          L"" ,       L"|" , L""  ,
+          L"" , L"-", L"|" , L"" ,
+
+          L"" ,       L"|", L""  ,
+          L"" , L"",  L"" , L"" ,
+
+          L"" , L"",  L"" , L"" ,
+        },
+        // width
+        {0,1,0},{0,1,0,0},
+      }
+    },
+    {
+      "ascii light header/separator/border",
+      Style{
+        // charset
+        {
+          L"+" , L"-", L"+" , L"+" ,
+          L"|" ,       L"|" , L"|"  ,
+          L"+" , L"-", L"+" , L"|" ,
+
+          L"|" ,       L"|" , L"|"  ,
+          L"|" , L"",  L"|" , L"|" ,
+
+          L"+" , L"-", L"+" , L"+" ,
+        },
+        // width
+        {1,1,1},{1,1,0,1},
+      }
+    },
+    {
+      "ascii light separator/border",
+      Style{
+        // charset
+        {
+          L"+", L"-" , L"+" , L"+" ,
+          L"|",        L"|" , L"|"  ,
+          L"" , L"",   L""  , L"" ,
+
+          L"|" ,       L"|" , L"|"  ,
+          L"|" , L"",  L"|" , L"|" ,
+
+          L"+" , L"-", L"+" , L"+" ,
+        },
+        // width
+        {1,1,1},{1,0,0,1},
+      }
+    },
+    {
+      "ascii light border",
+      Style{
+        // charset
+        {
+          L"+" , L"-" , L"-" , L"+" ,
+          L"|",        L" " , L"|"  ,
+          L"" , L"",   L""  , L"" ,
+
+          L"|" ,       L" " , L"|"  ,
+          L"|" , L"",  L" " , L"|" ,
+
+          L"+" , L"-", L"-" , L"+" ,
+        },
+        // width
+        {1,1,1},{1,0,0,1},
+      }
+    },
+    {
+      "unicode",
+      Style{
+        // charset
+        {
+          L"┌" , L"─" , L"┬" , L"┐" ,
+          L"│"        , L"│" , L"│" ,
+          L"├" , L"─" , L"┼" , L"┤" ,
+
+          L"│"        , L"│" , L"│" ,
+          L"├" , L"─" , L"┼" , L"┤" ,
+
+          L"└" , L"─" , L"┴" , L"┘" ,
+        },
+        // width
+        {1,1,1},{1,1,1,1},
+      }
+    },
+    {
+      "unicode rounded",
+      Style{
+        // charset
+        {
+          L"╭" , L"─" , L"┬" , L"╮" ,
+          L"│"        , L"│" , L"│" ,
+          L"├" , L"─" , L"┼" , L"┤" ,
+
+          L"│"        , L"│" , L"│" ,
+          L"├" , L"─" , L"┼" , L"┤" ,
+
+          L"╰" , L"─" , L"┴" , L"╯" ,
+        },
+        // width
+        {1,1,1},{1,1,1,1},
+      }
+    },
+    {
+      "unicode bold",
+      Style{
+        // charset
+        {
+          L"┏" , L"━" , L"┳" , L"┓" ,
+          L"┃"        , L"┃" , L"┃" ,
+          L"┣" , L"━" , L"╋" , L"┫" ,
+
+          L"┃"        , L"┃" , L"┃" ,
+          L"┣" , L"━" , L"╋" , L"┫" ,
+
+          L"┗" , L"━" , L"┻" , L"┛" ,
+        },
+        // width
+        {1,1,1},{1,1,1,1},
+      }
+    },
+    {
+      "unicode double",
+      Style{
+        // charset
+        {
+          L"╔" , L"═" , L"╦" , L"╗" ,
+          L"║"        , L"║" , L"║" ,
+          L"╠" , L"═" , L"╬" , L"╣" ,
+
+          L"║"        , L"║" , L"║" ,
+          L"╠" , L"═" , L"╬" , L"╣" ,
+
+          L"╚" , L"═" , L"╩" , L"╝" ,
+        },
+        // width
+        {1,1,1},{1,1,1,1},
+      }
+    },
+    {
+      "unicode with bold header",
+      Style{
+        // charset
+        {
+          L"┏" , L"━" , L"┳" , L"┓" ,
+          L"┃"        , L"┃" , L"┃" ,
+          L"┡" , L"━" , L"╇" , L"┩" ,
+
+          L"│"        , L"│" , L"│" ,
+          L"├" , L"─" , L"┼" , L"┤" ,
+
+          L"└" , L"─" , L"┴" , L"┘" ,
+        },
+        // width
+        {1,1,1},{1,1,1,1},
+      }
+    },
+    {
+      "unicode with double header",
+      Style{
+        // charset
+        {
+          L"╒" , L"═" , L"╤" , L"╕" ,
+          L"│"        , L"│" , L"│" ,
+          L"╞" , L"═" , L"╪" , L"╡" ,
+
+          L"│"        , L"│" , L"│" ,
+          L"├" , L"─" , L"┼" , L"┤" ,
+
+          L"└" , L"─" , L"┴" , L"┘" ,
+        },
+        // width
+        {1,1,1},{1,1,1,1},
+      }
+    },
+    {
+      "unicode cells",
+      Style{
+        // charset
+        {
+          L"╭"  , L"─"  , L"╮╭"   , L"╮"  ,
+          L"│"          , L"││"   , L"│"  ,
+          L"╰╭" , L"──" , L"╯╰╮╭" , L"╯╮" ,
+
+          L"│"          , L"││"   , L"│"  ,
+          L"╰╭" , L"──" , L"╯╰╮╭" , L"╯╮" ,
+
+          L"╰"  , L"─"  , L"╯╰"   , L"╯"  ,
+        },
+        // width
+        {1,2,1},{1,2,2,1},
+      }
+    },
+    {
+      "unicode cells 2",
+      Style{
+        // charset
+        {
+          L"╭─│╭"  , L"──"  , L"──╮╭"   , L"─╮╮│"  ,
+          L"││"             , L"││" , L"││"    ,
+          L"│╰│╭" , L"──" , L"╯╰╮╭" , L"╯│╮│" ,
+
+          L"││"  , L"││" , L"││"    ,
+          L"│╰│╭" , L"──" , L"╯╰╮╭" , L"╯│╮│" ,
+
+          L"│╰╰─"  , L"──"  , L"╯╰──"   , L"╯│─╯"  ,
+        },
+        // width
+        {2,2,2},{2,2,2,2},
+      }
+    },
+    {
+      "conceptual",
+      Style{
+        // charset
+        {
+          L" " , L"_" , L"  " , L" " ,
+          L"/"        , L"\\/" , L"\\" ,
+          L"\\" , L"_" , L"/\\" , L"/" ,
+
+          L"/"        , L"\\/" , L"\\" ,
+          L"\\" , L"_" , L"/\\" , L"/" ,
+
+          L"\\" , L"_" , L"/\\" , L"/" ,
+        },
+        // width
+        {1,2,1},{1,1,1,1},
+      }
+    },
+  };
+};
+// clang-format on
+
+class Table : public Translator {
+ public:
+  virtual ~Table() = default;
+  virtual std::string operator()(const std::string& input,
+                                 const std::string& options_string) {
+    auto options = SerializeOption(options_string);
+
+    // Style.
+    std::string style_option = options["style"];
+    Style style = styles["ascii"];
+    if (styles.count(style_option)) {
+      style = styles[style_option];
+    }
+
+    // Separator.
+    std::wstring separator = to_wstring(options["separator"]);
+    if (separator.size() != 1) {
+      separator = U',';
+    }
+
+    // Parse data.
+    std::vector<std::vector<std::wstring>> data;
+    std::wstring line;
+    std::wstringstream ss(to_wstring(input));
+    while (std::getline(ss, line)) {
+      data.emplace_back();
+      std::wstring cell;
+      std::wstringstream ss_line(line);
+      while (std::getline(ss_line, cell, separator[0])) {
+        data.back().push_back(cell);
+      }
+    }
+
+    // Compute row/line count.
+    int line_count = data.size();
+    int row_count = 0;
+    for (const auto& line : data) {
+      row_count = std::max(row_count, (int)line.size());
+    }
+
+    // Uniformize the number of cells per lines.
+    for (auto& line : data) {
+      line.resize(row_count);
+    }
+
+   
+    std::cout << int(L"┌"[0]) << std::endl;
+
+    // Compute row_width;
+    std::vector<int> row_width(row_count, 0);
+    for (const auto& line : data) {
+      for(int i = 0; i<line.size(); ++i) {
+        row_width[i] = std::max(row_width[i], (int)line[i].size());
+      }
+    }
+
+    // Compute sum_row_width;
+    int row_width_global = 0;
+    for(const auto it : row_width) {
+      row_width_global += it;
+    }
+
+    // Compute screen dimension.
+    int width = style.width[0] + style.width[1] * (row_count - 1) +
+                style.width[2] + row_width_global;
+    int height = style.height[0] + style.height[1] +
+                 style.height[2] * (line_count - 2) + style.height[3] + line_count;
+
+    Screen screen(width, height);
+    std::cerr << "width = " << width << " height = " << height << std::endl;
+
+    // Draw table.
+    int Y = 0;
+    for (int y = 0; y < line_count; ++y) {
+      bool last_line = (y == line_count - 1);
+      int X = 0;
+
+      const int cell_top = Y + style.height[std::min(2,y)];
+      const int cell_bottom = cell_top + 1;
+
+      for (int x = 0; x < data[y].size(); ++x) {
+        bool last_row = (x == row_count - 1);
+        // clang-format off
+        const int top_char = 
+          y == 0 ? 1 :
+          y == 1 ? 8 :
+                   15;
+        const int left_char =
+          y == 0 ? (x == 0 ? 4 : 5) :
+                   (x == 0 ? 11:12) ;
+        const int right_char = 
+          y == 0 ? 6:
+                   13;
+        const int bottom_char = 19;
+        const int top_left_char =
+            y == 0 ? (x == 0 ? 0 : 2):
+            y == 1 ? (x == 0 ? 7 : 9):
+                     (x == 0 ? 14:16);
+        const int top_right_char =
+            y == 0 ? 3 :
+            y == 1 ? 10:
+                     17;
+        const int bottom_left_char =
+            x == 0 ? 18:
+                     20;
+        const int bottom_right_char = 21;
+
+        const int cell_left = X + style.width[std::min(1,x)];
+        const int cell_right = cell_left + row_width[x];
+
+        // clang-format on
+
+        // Draw Top.
+        {
+          int i = 0;
+          for (int yy = Y; yy < cell_top; ++yy) {
+            for (int xx = cell_left; xx < cell_right; ++xx) {
+              screen.DrawPixel(xx, yy, style.charset[top_char][i]);
+            }
+            ++i;
+          }
+        }
+        // Draw Down.
+        if (last_line) {
+          int i = 0;
+          for (int yy = cell_bottom; yy < height; ++yy) {
+            for (int xx = cell_left; xx < cell_right; ++xx) {
+              screen.DrawPixel(xx, yy, style.charset[bottom_char][i]);
+            }
+            ++i;
+          }
+        }
+
+        // Draw Left.
+        for(int yy = cell_top; yy < cell_bottom; ++yy) {
+          screen.DrawText(X, yy, style.charset[left_char]);
+        }
+
+        // Draw Right.
+        if (last_row) {
+          for (int yy = cell_top; yy < cell_bottom; ++yy) {
+            screen.DrawText(cell_right, yy, style.charset[right_char]);
+          }
+        }
+
+        // Draw Left/Top
+        {
+          int i = 0;
+          for(int yy = Y; yy<cell_top; ++yy) {
+            for(int xx = X; xx<cell_left; ++xx) {
+              screen.DrawPixel(xx,yy,style.charset[top_left_char][i]);
+              ++i;
+            }
+          }
+        }
+
+        // Draw Right/Top
+        if (last_row) {
+          int i = 0;
+          for (int yy = Y; yy < cell_top; ++yy) {
+            for (int xx = cell_right; xx < width; ++xx) {
+              screen.DrawPixel(xx,yy,style.charset[top_right_char][i]);
+              ++i;
+            }
+          }
+        }
+
+        // Draw Left/Bottom
+        if (last_line)
+        {
+          int i = 0;
+          for (int yy = cell_bottom; yy < height; ++yy) {
+            for (int xx = X; xx < cell_left; ++xx) {
+              screen.DrawPixel(xx, yy, style.charset[bottom_left_char][i]);
+              ++i;
+            }
+          }
+        }
+
+        // Draw Right/Bottom
+        if (last_row && last_line)
+        {
+          int i = 0;
+          for(int yy = cell_bottom; yy<height; ++yy) {
+            for (int xx = cell_right; xx < width; ++xx) {
+              screen.DrawPixel(xx,yy,style.charset[bottom_right_char][i]);
+              ++i;
+            }
+          }
+        }
+        if (false) {
+        }
+
+        // Draw Text.
+        screen.DrawText(cell_left, cell_top, data[y][x]);
+        X = cell_right;
+      }
+      Y = cell_bottom;
+    }
+
+    return screen.ToString();
+  }
+};
+
+std::unique_ptr<Translator> TableTranslator() {
+  return std::make_unique<Table>();
+}
---
+++ src/translator/tree/Tree.cpp
@@ -0,0 +1,353 @@
+#include <functional>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+#include "screen/Screen.h"
+#include "translator/Translator.h"
+
+namespace {
+
+struct Node {
+  std::wstring content;
+  Node* parent = nullptr;
+  std::vector<std::unique_ptr<Node>> children;
+};
+
+struct Line {
+  int spaces = 0;
+  std::wstring content;
+  Node* tree;
+};
+
+std::string PrintUnicode1(std::unique_ptr<Node> tree) {
+  std::stringstream output;
+  std::function<void(std::unique_ptr<Node>&, std::string)> print_children;
+  print_children = [&](std::unique_ptr<Node>& node, std::string prefix) {
+    for (int i = 0; i < node->children.size(); ++i) {
+      auto& child = node->children[i];
+      if (i != node->children.size() - 1) {
+        output <<             prefix + " ├─" << to_string(child->content) << std::endl;
+        print_children(child, prefix + " │ ");
+      } else {
+        output <<             prefix + " └─" << to_string(child->content) << std::endl;
+        print_children(child, prefix + "   ");
+      }
+    }
+  };
+
+  for(auto& it : tree->children) {
+    output << to_string(it->content) << std::endl;
+    print_children(it, "");
+  }
+  return output.str();
+}
+
+
+std::string PrintUnicode2(std::unique_ptr<Node> tree) {
+  std::stringstream output;
+  std::function<void(std::unique_ptr<Node>&, std::string)> print_children;
+  print_children = [&](std::unique_ptr<Node>& node, std::string prefix) {
+    for (int i = 0; i < node->children.size(); ++i) {
+      auto& child = node->children[i];
+      if (i != node->children.size() - 1) {
+        output <<             prefix + " ├──" << to_string(child->content) << std::endl;
+        print_children(child, prefix + " │  ");
+      } else {
+        output <<             prefix + " └──" << to_string(child->content) << std::endl;
+        print_children(child, prefix + "    ");
+      }
+    }
+  };
+
+  for(auto& it : tree->children) {
+    output << to_string(it->content) << std::endl;
+    print_children(it, "");
+  }
+  return output.str();
+}
+
+std::string PrintASCII1(std::unique_ptr<Node> tree) {
+  std::stringstream output;
+  std::function<void(std::unique_ptr<Node>&, std::string)> print_children;
+  print_children = [&](std::unique_ptr<Node>& node, std::string prefix) {
+    for (int i = 0; i < node->children.size(); ++i) {
+      auto& child = node->children[i];
+      if (i != node->children.size() - 1) {
+        output <<             prefix + " +-" << to_string(child->content) << std::endl;
+        print_children(child, prefix + " |  ");
+      } else {
+        output <<             prefix + " `-" << to_string(child->content) << std::endl;
+        print_children(child, prefix + "    ");
+      }
+    }
+  };
+
+  for(auto& it : tree->children) {
+    output << to_string(it->content) << std::endl;
+    print_children(it, "");
+  }
+  return output.str();
+}
+
+std::string PrintASCII2(std::unique_ptr<Node> tree) {
+  std::stringstream output;
+  std::function<void(std::unique_ptr<Node>&, std::string)> print_children;
+  print_children = [&](std::unique_ptr<Node>& node, std::string prefix) {
+    for (int i = 0; i < node->children.size(); ++i) {
+      auto& child = node->children[i];
+      if (i != node->children.size() - 1) {
+        output <<             prefix + " +--" << to_string(child->content) << std::endl;
+        print_children(child, prefix + " |  ");
+      } else {
+        output <<             prefix + " `--" << to_string(child->content) << std::endl;
+        print_children(child, prefix + "    ");
+      }
+    }
+  };
+
+  for(auto& it : tree->children) {
+    output << to_string(it->content) << std::endl;
+    print_children(it, "");
+  }
+  return output.str();
+}
+
+std::string PrintASCII3(std::unique_ptr<Node> tree) {
+  std::stringstream output;
+  std::function<void(std::unique_ptr<Node>&, std::string)> print_children;
+  print_children = [&](std::unique_ptr<Node>& node, std::string prefix) {
+    for (int i = 0; i < node->children.size(); ++i) {
+      auto& child = node->children[i];
+      if (i != node->children.size() - 1) {
+        output <<             prefix + " |--" << to_string(child->content) << std::endl;
+        print_children(child, prefix + " |  ");
+      } else {
+        output <<             prefix + " `--" << to_string(child->content) << std::endl;
+        print_children(child, prefix + "    ");
+      }
+    }
+  };
+
+  for(auto& it : tree->children) {
+    output << to_string(it->content) << std::endl;
+    print_children(it, "");
+  }
+  return output.str();
+}
+
+struct DisplayTree {
+  int entrance;
+  std::vector<std::wstring> content;
+};
+
+enum class Align {
+  Top,
+  Center,
+  Bottom,
+};
+
+DisplayTree MergeDisplayTree(std::wstring content,
+                             std::vector<DisplayTree> children,
+                             Align align) {
+  std::wstring space_to_add(content.size() + 3, U' ');
+  DisplayTree ret;
+
+  // Leaf of the recursion.
+  if (children.size() == 0) {
+    ret.content.push_back(content);
+    ret.entrance = 0;
+    return ret;
+  }
+
+  // Draw Childrens
+  for (auto& child : children) {
+    for (auto& line : child.content) {
+      ret.content.push_back(space_to_add + line);
+    }
+  }
+
+  // Draw the current content.
+  switch (align) {
+    case Align::Top:
+      ret.entrance = 0;
+      break;
+    case Align::Center:
+      ret.entrance = ret.content.size() / 2;
+      break;
+    case Align::Bottom:
+      ret.entrance = ret.content.size() - 1;
+      break;
+  };
+
+  for(int i = 0; i<content.size(); ++i) {
+    ret.content[ret.entrance][i] = content[i];
+  }
+
+  // Draw vertex.
+  int first_entrance = children[0].entrance;
+  int last_entrance = 0;
+  {
+    int y = 0;
+    for (auto& child : children) {
+      last_entrance = y + child.entrance;
+      y += child.content.size();
+    }
+  }
+
+  int y = 0;
+  for(auto& child : children) {
+    int start = y;
+
+    // Draw Child vertical connector.
+    for (auto& line : child.content) {
+      if (y >= first_entrance && y <= last_entrance) {
+        ret.content[y][content.size() + 1] = U'│';
+      }
+      ++y;
+    }
+
+    // Refine connector on child entrance points.
+    int child_entrance = start + child.entrance;
+    auto& connector = ret.content[child_entrance][content.size() + 1];
+    if (first_entrance == last_entrance)
+      connector = U'─';
+    else if (child_entrance == first_entrance)
+      connector = U'┌';
+    else if (child_entrance < last_entrance)
+      connector = U'├';
+    else
+      connector = U'└';
+
+    // Draw connector to child entrance.
+    ret.content[child_entrance][content.size() + 2] = U'─';
+  }
+
+  // Draw parent entrance to connector.
+  ret.content[ret.entrance][content.size() + 0] = U'─';
+  // Refine connector on parent entrance points.
+  auto& connector = ret.content[ret.entrance][content.size() + 1];
+  switch (connector) {
+    case U'─': connector = U'─'; break;
+    case U'┌': connector = U'┬'; break;
+    case U'├': connector = U'┼'; break;
+    case U'└': connector = U'┴'; break;
+    case U'│': connector = U'┤'; break;
+  }
+
+  return ret;
+}
+
+DisplayTree MakeDisplayTree(Node* tree, Align align) {
+  std::vector<DisplayTree> children_tree;
+  int i = 0;
+  for (auto& child : tree->children) {
+    children_tree.push_back(MakeDisplayTree(child.get(), align));
+  }
+  return MergeDisplayTree(tree->content, std::move(children_tree), align);
+}
+
+std::string PrintUnicodeRightTop(std::unique_ptr<Node> tree) {
+  DisplayTree display = MakeDisplayTree(tree.get(), Align::Top);
+  std::stringstream output;
+  for(auto& line : display.content) {
+    output << to_string(line) << std::endl;
+  }
+  return output.str();
+}
+
+std::string PrintUnicodeRightCenter(std::unique_ptr<Node> tree) {
+  DisplayTree display = MakeDisplayTree(tree.get(), Align::Center);
+  std::stringstream output;
+  for(auto& line : display.content) {
+    output << to_string(line) << std::endl;
+  }
+  return output.str();
+}
+
+std::string PrintUnicodeRightBottom(std::unique_ptr<Node> tree) {
+  DisplayTree display = MakeDisplayTree(tree.get(), Align::Bottom);
+  std::stringstream output;
+  for(auto& line : display.content) {
+    output << to_string(line) << std::endl;
+  }
+  return output.str();
+}
+
+std::map<std::string, std::function<std::string(std::unique_ptr<Node>)>>
+    print_function = {
+        {"unicode 1", PrintUnicode1},
+        {"unicode 2", PrintUnicode2},
+        {"ASCII 1", PrintASCII1},
+        {"ASCII 2", PrintASCII2},
+        {"ASCII 3", PrintASCII3},
+        {"unicode right top", PrintUnicodeRightTop},
+        {"unicode right center", PrintUnicodeRightCenter},
+        {"unicode right bottom", PrintUnicodeRightBottom},
+};
+
+} // namespace
+
+class Tree : public Translator {
+ public:
+  virtual ~Tree() = default;
+  virtual std::string operator()(const std::string& input,
+                                 const std::string& options_string) {
+    auto options = SerializeOption(options_string);
+
+    // Style.
+    std::string style_option = options["style"];
+
+    // Parse the tree.
+    std::vector<Line> lines;
+
+    std::string line_text;
+    std::stringstream ss(input);
+    while (std::getline(ss, line_text)) {
+      Line line;
+      line.content = to_wstring(line_text);
+      while (line.spaces < line.content.size() &&
+             (line.content[line.spaces] == U' ' ||
+              line.content[line.spaces] == U'\t')) {
+        line.spaces++;
+      }
+      line.content = line.content.substr(line.spaces, -1);
+      lines.push_back(line);
+    }
+
+    if (lines.size() == 0)
+      return "";
+    
+    // Build the tree.
+    auto tree = std::make_unique<Node>();
+    for(int i = 0; i<lines.size(); ++i) {
+      auto child = std::make_unique<Node>();
+      lines[i].tree = child.get();
+      child->content = lines[i].content;
+      for (int j = i - 1; ; --j) {
+
+        if (j == -1) {
+          child->parent = tree.get();
+          child->parent->children.push_back(std::move(child));
+          break;
+        }
+
+        if (lines[j].spaces < lines[i].spaces) {
+          child->parent = lines[j].tree;
+          child->parent->children.push_back(std::move(child));
+          break;
+        }
+      }
+    }
+
+    if (print_function.count(style_option)) {
+      return print_function[style_option](std::move(tree));
+    } else {
+      return print_function["unicode 2"](std::move(tree));
+    }
+  }
+};
+
+std::unique_ptr<Translator> TreeTranslator() {
+  return std::make_unique<Tree>();
+}
