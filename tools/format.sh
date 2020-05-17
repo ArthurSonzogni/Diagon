0f5fec894f25fd7aa00e590595100256214adbc6
ArthurSonzogni <sonzogniarthur@gmail.com>
2020-05-17 13:40:09 +0200

    Fix Window cmake.

---
+++ tools/format.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+cd ..
+
+# Add the license file.
+files=$(find ./src ./test -name "*.hpp" -o -name "*.cpp")
+for file in $files
+do
+  if ! grep -q Copyright $file
+  then
+    cat ./tools/license_headers.cpp $file >$file.new && mv $file.new $file
+  fi
+done
+
+# Use clang-format.
+for file in $files
+do
+  clang-format -i $file
+done
