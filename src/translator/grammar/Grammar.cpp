66dabda26ea7112d14d9db3a12ae26db4d2cc72c
ArthurSonzogni <sonzogniarthur@gmail.com>
2020-11-08 15:28:30 +0100

    Put kgt behind a namespace.

--- src/translator/grammar/Grammar.cpp
+++ src/translator/grammar/Grammar.cpp
@@ -4,38 +4,38 @@
 
 #include "translator/grammar/Grammar.hpp"
 
-#include "screen/Screen.h"
+#include <unistd.h>
+#include <fstream>
 #include <iostream>
 #include <sstream>
-#include <fstream>
-#include <unistd.h>
+#include "screen/Screen.h"
 
+namespace kgt {
 extern "C" {
 int debug = 0;
 int prettify = 1;
 int allow_undefined = 1;
 const char* css_file = nullptr;
 #include "abnf/io.h"
-#include "rrtext/io.h"
-#include "bnf/io.h"
 #include "blab/io.h"
+#include "bnf/io.h"
+#include "dot/io.h"
 #include "ebnfhtml5/io.h"
-#include "wsn/io.h"
-#include "abnf/io.h"
+#include "html5/io.h"
 #include "iso-ebnf/io.h"
 #include "rbnf/io.h"
-#include "sid/io.h"
-#include "dot/io.h"
 #include "rrdot/io.h"
 #include "rrdump/io.h"
-#include "rrtdump/io.h"
-#include "rrparcon/io.h"
 #include "rrll/io.h"
+#include "rrparcon/io.h"
 #include "rrta/io.h"
+#include "rrtdump/io.h"
 #include "rrtext/io.h"
+#include "sid/io.h"
 #include "svg/io.h"
-#include "html5/io.h"
+#include "wsn/io.h"
 }
+}  // namespace kgt
 
 const char* Grammar::Name() {
   return "Grammar";
@@ -62,26 +62,10 @@ std::vector<Translator::OptionDescription> Grammar::Options() {
       {
           "output",
           {
-              "bnf",
-              "blab",
-              "ebnfhtml5",
-              "ebnfxhtml5",
-              "wsn",
-              "abnf",
-              "iso-ebnf",
-              "rbnf",
-              "sid",
-              "dot",
-              "rrdot",
-              "rrdump",
-              "rrtdump",
-              "rrparcon",
-              "rrll",
-              "rrta",
-              "rrtext",
-              "rrutf8",
-              "svg",
-              "html5",
+              "bnf",    "blab",     "ebnfhtml5", "ebnfxhtml5", "wsn",
+              "abnf",   "iso-ebnf", "rbnf",      "sid",        "dot",
+              "rrdot",  "rrdump",   "rrtdump",   "rrparcon",   "rrll",
+              "rrta",   "rrtext",   "rrutf8",    "svg",        "html5",
               "xhtml5",
           },
           "rrutf8",
@@ -114,46 +98,46 @@ struct StringReader {
   int index = 0;
 };
 
-using AstRulePtr = ast_rule*;
+using AstRulePtr = kgt::ast_rule*;
 using Opaque = void*;
 using OpaqueRead = int(Opaque);
 using InputFunction = AstRulePtr(OpaqueRead, Opaque);
-using OutputFunction = void(const struct ast_rule *);
+using OutputFunction = void(const struct kgt::ast_rule*);
 
-InputFunction* f = abnf_input;
+InputFunction* f = kgt::abnf_input;
 
 std::map<std::string, InputFunction*> input_function_map = {
-    {"abnf", abnf_input},
+    {"abnf", kgt::abnf_input},
     //
-    {"bnf", bnf_input},
-    {"wsn", wsn_input},
-    {"iso-ebnf", iso_ebnf_input},
-    {"rbnf", rbnf_input},
+    {"bnf", kgt::bnf_input},
+    {"wsn", kgt::wsn_input},
+    {"iso-ebnf", kgt::iso_ebnf_input},
+    {"rbnf", kgt::rbnf_input},
 };
 
 std::map<std::string, OutputFunction*> output_function_map = {
-    {"rrutf8", rrutf8_output},
+    {"rrutf8", kgt::rrutf8_output},
     //
-    {"bnf", bnf_output},
-    {"blab", blab_output},
-    {"ebnfhtml5", ebnf_html5_output},
-    {"ebnfxhtml5", ebnf_xhtml5_output},
-    {"wsn", wsn_output},
-    {"abnf", abnf_output},
-    {"iso-ebnf", iso_ebnf_output},
-    {"rbnf", rbnf_output},
-    {"sid", sid_output},
-    {"dot", dot_output},
-    {"rrdot", rrdot_output},
-    {"rrdump", rrdump_output},
-    {"rrtdump", rrtdump_output},
-    {"rrparcon", rrparcon_output},
-    {"rrll", rrll_output},
-    {"rrta", rrta_output},
-    {"rrtext", rrtext_output},
-    {"svg", svg_output},
-    {"html5", html5_output},
-    {"xhtml5", xhtml5_output},
+    {"bnf", kgt::bnf_output},
+    {"blab", kgt::blab_output},
+    {"ebnfhtml5", kgt::ebnf_html5_output},
+    {"ebnfxhtml5", kgt::ebnf_xhtml5_output},
+    {"wsn", kgt::wsn_output},
+    {"abnf", kgt::abnf_output},
+    {"iso-ebnf", kgt::iso_ebnf_output},
+    {"rbnf", kgt::rbnf_output},
+    {"sid", kgt::sid_output},
+    {"dot", kgt::dot_output},
+    {"rrdot", kgt::rrdot_output},
+    {"rrdump", kgt::rrdump_output},
+    {"rrtdump", kgt::rrtdump_output},
+    {"rrparcon", kgt::rrparcon_output},
+    {"rrll", kgt::rrll_output},
+    {"rrta", kgt::rrta_output},
+    {"rrtext", kgt::rrtext_output},
+    {"svg", kgt::svg_output},
+    {"html5", kgt::html5_output},
+    {"xhtml5", kgt::xhtml5_output},
 };
 // clang-format on
 
@@ -168,7 +152,7 @@ std::string Grammar::Translate(const std::string& input,
   FILE* file_write = fopen("/tmp/diagon_grammer.txt", "w");
   dup2(fileno(file_write), 1);
 
-  struct ast_rule* model = nullptr;
+  kgt::ast_rule* model = nullptr;
 
   // Read string to model.
   auto string_reader = StringReader(input);
@@ -180,10 +164,10 @@ std::string Grammar::Translate(const std::string& input,
 
   auto input_function = input_function_map.count(option_input)
                             ? input_function_map[option_input]
-                            : abnf_input;
+                            : kgt::abnf_input;
   auto output_function = output_function_map.count(option_output)
                              ? output_function_map[option_output]
-                             : rrutf8_output;
+                             : kgt::rrutf8_output;
 
   model = input_function(StringReader::Read, &string_reader);
   output_function(model);
